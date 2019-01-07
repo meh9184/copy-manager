d8167c380abce605bff8d83288769fa5fc85b7b1
Eunhwan Moon <meh9184@naver.com>
2019-01-07 10:13:40 +0900

    git repository init

---
+++ bg_hu_mini.js
@@ -0,0 +1,39 @@
+var copiedText = null;
+var allClips=[];
+
+chrome.runtime.onMessage.addListener(function(a, c, b) {
+        if(a.event=="copy"){
+            copiedText = a.text;
+            allClips.push(copiedText);
+            d = allClips.length;
+            if (d>25) {
+                e = d-25;
+                allClips.splice(0,e);
+            }
+            alert(copiedText);
+            alert(allClips);
+
+            assignBadge()
+        }
+        b({copiedText:copiedText})
+    }
+);
+
+function assignBadge(){
+    var a = allClips.length;
+
+    // alert("new badge:"+ a);
+    chrome.browserAction.setBadgeText({text:""+a});
+    chrome.browserAction.setTitle({title:a+"  clips"})
+}
+
+function getAllClips(){
+    return allClips
+}
+function removeClip(a){
+    allClips.splice(a,1);
+    assignBadge();
+}
+function returnClip(a){
+    return allClips[a];
+};
---
+++ clip_hu_mini.js
@@ -0,0 +1,95 @@
+ var oTable ;
+ $(document).ready(function(){
+	 oTable = $('#cbtable').DataTable(
+		 {
+			 sPaginationType:"full_numbers",
+			 bSort:false,
+			 oLanguage:
+				 {
+					 oPaginate:
+						 {
+							 sLast:">>",
+							 sFirst:"<<",
+							 sPrevious:"<",
+							 sNext:">"
+						 },
+					 sZeroRecords:"No records to display",
+					 sInfoEmpty:"No entries to show",
+					 sEmptyTable:"No clip found. <br/>Please copy some web content."
+				 },
+			 aLengthMenu:
+				 [
+					 [5,10,15,20,-1],
+					 [5,10,15,20,"All"]
+				 ]
+		 });
+    
+	
+ /*
+oTable=$("#cbtable").dataTable({sPaginationType:"full_numbers",bSort:false,oLanguage:{oPaginate:{sLast:">>",sFirst:"<<",sPrevious:"<",sNext:">"},sZeroRecords:"No records to display",sInfoEmpty:"No entries to show",sEmptyTable:"No clip found. <br/>Please copy some web content."},aLengthMenu:[[5,10,15,20,-1],[5,10,15,20,"All"]]})
+*/
+
+	 $("body").on("click",".delete",function(){
+		 var J=$(this).attr('remid');
+		 getBackground().removeClip(J+"");
+		 $(this).parent().parent().remove();
+		 //displayRecords();
+	 });
+
+	 $("body").on("click",".copyme",function(){
+		 copyMe($(this).attr('id'));
+	 });
+
+	 displayRecords();
+
+ });
+
+function displayRecords(){
+	var clipList=getBackground().getAllClips(); 
+	var len=clipList.length;
+	if (len>0){	$("#record").html("");}			
+	for(var i=len-1; i>=0; i--) {
+		var value =getHTMLEncode(clipList[i]);
+		var col1="<div id='"+i+"' class='copyme multiple' alt='Click to copy' title='Click to copy'>"+value+"</div>";
+		var col2="<img src='remove.png' class='delete' alt='Remove from clipboard' remid='"+i+"' title='Remove from clipboard'/>";
+		//value="<tr></td><td class='center'><</td></tr>";
+		//$("#cbtable tbody").append(value);
+		oTable.row.add([col1,col2]).draw();
+	}
+}
+
+function copyMe(j){
+	bg=getBackground();
+	clipboardholder=bg.document.getElementById("clipboardholder");
+	clipboardholder.style.display="block";
+	clipboardholder.value=bg.returnClip(j);
+	clipboardholder.select();
+	bg.document.execCommand("Copy");
+	clipboardholder.style.display="none";
+	topBar("Content copied to clipboard!",j)
+}
+
+function getHTMLEncode(j) {
+	return j.toString()
+		.replace(/&/g,"&amp;")
+		.replace(/"/g,"&quot;")
+		.replace(/</g,"&lt;")
+		.replace(/>/g,"&gt;")
+}
+
+function getBackground(){
+	return chrome.extension.getBackgroundPage()
+}
+
+function topBar(j,J){
+
+	var n="#"+J;
+	$("<div />",{"class":"topbar",text:j})
+		.hide()
+		.prependTo(n)
+		.slideDown("fast")
+		.delay(700)
+		.slideUp(function(){
+			$(this).remove()
+		})
+};
