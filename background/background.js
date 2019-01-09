be040d5527468f5962120619868660f3ea42e009
Eunhwan Moon <meh9184@naver.com>
2019-01-09 19:44:09 +0900

    screenshots 추가

--- background/background.js
+++ background/background.js
@@ -1,6 +1,30 @@
 var copiedText = null;
 var allClips=[];
 
+// 사이드바 열려있는지 체크
+var is_open = false;
+whale.sidebarAction.onClicked.addListener(function(result) {
+    if(result.opened) {
+        is_open = true;
+    } else {
+        is_open = false;
+    }
+});
+
+// 단축키
+whale.commands.onCommand.addListener(function (command) {
+     // 사이드바 on/off 단축키
+    if(command === "sidebar_action") {
+        if(is_open) {
+            whale.sidebarAction.hide();
+            is_open = false;
+        } else {
+            whale.sidebarAction.show();
+            is_open = true;
+        }
+    }
+});
+
 chrome.runtime.onMessage.addListener(function(a, c, b) {
         if(a.event=="copy"){
             copiedText = a.text;
